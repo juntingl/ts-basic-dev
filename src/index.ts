0a04a57d1eb9f2679031d5cfc22c592523e69124
Junting <342766475@qq.com>
2020-05-24 13:12:24 +0800

    使用发布的组件：vue-simple-query-component

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,26 @@
 import Vue from 'vue';
-import Hello from '@/components/Hello.vue';
+// import Hello from '@/components/Hello.vue';
+import EmployeeQuery from 'vue-simple-query-component';
 
 new Vue({
   el: '.app',
   // template: `<h1>Hello {{name}}</h1>`,
-  render: h => h(Hello)
+  // render: h => h(Hello)
+  components: {
+    EmployeeQuery
+  },
+  template: `<query-component @query="getParam" :department="department" />`,
+  data: {
+    department: [
+      { department: '技术部', departmentId: 1 },
+      { department: '产品部', departmentId: 2 },
+      { department: '市场部', departmentId: 3 },
+      { department: '运营部', departmentId: 4 }
+    ]
+  },
+  methods: {
+    getParam(params: any) {
+      console.log("getParam -> params", params)
+    }
+  }
 });
